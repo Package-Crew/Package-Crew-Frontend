9879d4bfb2ad7ec7828d2e484f736edf7e44d810
Dawon00 <dawon000203@gmail.com>
2023-08-29 19:17:14 +0900

    fix: 버그 수정

--- package-crew/src/App.js
+++ package-crew/src/App.js
@@ -1,18 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./components/Header";
+import Home from "./pages/home";
+import Header from "./components/header";
 
 function App() {
-  return (
-    <div className="w-full mx-auto lg:w-[1024px]">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="w-full mx-auto lg:w-[1024px]">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
